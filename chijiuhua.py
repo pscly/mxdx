ff73bdaa848e9420bc97bb3598c2a06efbf8d3ca
pscly <pscly@outlook.com>
2021-05-12 10:38:37 +0800

    增加了判断json文件是否存在

--- chijiuhua.py
+++ chijiuhua.py
@@ -3,6 +3,7 @@
 # 数据保存到哪
 
 from conn_mongo import MyMongo1
+import os
 import json
 
 def drop_mongo(table=None, mongo1=None):
@@ -46,18 +47,22 @@ def to_json(data, table):
     '''
     保存到json文件
     data: 要保存的数据，格式:[{}]
-    table: 文件名(数据的类名)
+    table: 文件名(数据的类名), 应该是字符串
     '''
-    with open('./data/'+table, 'w', encoding='utf-8') as f:
+    with open(f'./data/{table}.json', 'w', encoding='utf-8') as f:
         json.dump(data, f)
 
 def load_json(table, tj={}):
     '''
     从json文件中读取数据
-    table: 要读取的表名
+    table: 要读取的表名, 应该是字符串
     tj: 读取的条件
     '''
-    with open('./data/'+table, 'r', encoding='utf-8') as f:
+    if not os.path.isfile(f'./data/{table}.json'):
+        print('建议先存入一些数据')
+        raise Exception(f'{table}数据不存在')
+
+    with open(f'./data/{table}.json', 'r', encoding='utf-8') as f:
         return json.load(f)
 
 
@@ -65,4 +70,8 @@ if __name__ == '__main__':
     # # to_mongo([{'_id': '1', 'name': 'pscly', 'price': 310}, {'_id': '2', 'name': 'lsy', 'price': 31.01}], 'test1')
     # x1 = load_mongo('test1')
     # print(x1)
+    # to_json([{'_id': '1', 'name': 'pscly', 'price': 310}, {'_id': '2', 'name': 'lsy', 'price': 31.01}], 'xx1')
     
+    x1 = load_json('xx21')
+    print(x1)
+
