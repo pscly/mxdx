9ce539d15ebbe21bcc02a19d694a6aef52849a80
pscly <pscly@outlook.com>
2021-05-12 09:51:15 +0800

    更新1

--- chijiuhua.py
+++ chijiuhua.py
@@ -3,6 +3,7 @@
 # 数据保存到哪
 
 from conn_mongo import MyMongo1
+import json
 
 def drop_mongo(table=None, mongo1=None):
     '''
@@ -31,7 +32,6 @@ def to_mongo(data, table):
     else:
         mongo1.save([data])
 
-
 def load_mongo(table, tj={}):
     '''
     table: 表名
@@ -42,7 +42,27 @@ def load_mongo(table, tj={}):
 
     return mongo1.find(tj)
 
+def to_json(data, table):
+    '''
+    保存到json文件
+    data: 要保存的数据，格式:[{}]
+    table: 文件名(数据的类名)
+    '''
+    with open('./data/'+table, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+def load_json(table, tj={}):
+    '''
+    从json文件中读取数据
+    table: 要读取的表名
+    tj: 读取的条件
+    '''
+    with open('./data/'+table, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
 if __name__ == '__main__':
-    # to_mongo([{'_id': '1', 'name': 'pscly', 'price': 310}, {'_id': '2', 'name': 'lsy', 'price': 31.01}], 'test1')
-    x1 = load_mongo('test1')
-    print(x1)
+    # # to_mongo([{'_id': '1', 'name': 'pscly', 'price': 310}, {'_id': '2', 'name': 'lsy', 'price': 31.01}], 'test1')
+    # x1 = load_mongo('test1')
+    # print(x1)
+    
